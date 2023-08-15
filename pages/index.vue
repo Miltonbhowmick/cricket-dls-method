cd5ac14bd369cab2b3c4b99b30f759884aecc7ac
miltonbhowmick <milton_bhowmick@yahoo.com>
2023-08-15 22:04:03 +0600

    Added some inputs for working on a single case

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,40 @@
 <template>
-	<h1>Cricket</h1>
+	<div class="main py-50">
+		<h1>Cricket DLS <abbr>(Duckworth Lewis Stern)</abbr> method</h1>
+		<section class="section-cases">
+			<div class="cases">
+				<div class="box">
+					<div class="inputs">
+						<input type="text" placeholder="team1 score" />
+						<input type="text" placeholder="team1 score" />
+						<input type="text" placeholder="team1 score" />
+					</div>
+					<button>submit</button>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
-<script></script>
-<style scoped></style>
+<script setup></script>
+<style scoped lang="scss">
+.main {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	.section-cases {
+		.cases {
+			display: flex;
+			flex-direction: column;
+			gap: 10px;
+			.inputs {
+				display: flex;
+				flex-wrap: wrap;
+				input {
+					flex-basis: 48%;
+				}
+			}
+		}
+	}
+}
+</style>
