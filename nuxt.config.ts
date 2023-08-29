b9c167d2ac68d960db4c10c9d127b1c57313c6ff
miltonbhowmick <miltonbhowmick6@gmail.com>
2023-08-29 10:35:36 +0600

    Added meta tags for the projects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+	app:{
+		head:{
+			title: 'DLS',
+			titleTemplate: "Cricket | %s",
+			meta:[
+				{charset: 'utf-8'},
+				{
+					hid: "viewport",
+					name: "viewport",
+					content: "width=device-width, initial-scale=1",
+				},
+				{ hid: "description", property: "description", content: "" },
+				{ property: "format-detection", content: "telephone=no" },
+				{
+					hid: "og:type",
+					property: "og:type",
+					content: "",
+				},
+				{
+					hid: "og:title",
+					property: "og:title",
+					content: "",
+				},
+				{
+					hid: "og:description",
+					property: "og:description",
+					content: "",
+				},
+				{
+					hid: "og:url",
+					property: "og:url",
+					content: "",
+				},
+				{
+					hid: "og:site_name",
+					property: "og:site_name",
+					content: "",
+				},
+				{
+					hid: "Keywords",
+					property: "Keywords",
+					content: "",
+				},
+				{
+					hid: "Description",
+					property: "Description",
+					content: "",
+				},
+			],
+			link:[
+				{ rel: "icon", type: "image/x-icon", href: "/images/cricket_favicon.png" },
+			]
+		}
+	},
   pinia: {
     autoImports: [
 			"defineStore",
